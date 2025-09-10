e745c9c7ea6c3f30dce23f1dc2ba5deb6fc95b56
Zenixalarate01 <dylan.pirade@ui.ac.id>
2025-09-10 14:23:45 +0700

    Tutorial 2 Complete

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,6 +35,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dylan-pirade-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "<https://dylan-pirade-footballnews.pbp.cs.ui.ac.id>"
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -62,7 +66,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
